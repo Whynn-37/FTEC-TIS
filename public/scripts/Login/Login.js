132bfe034d4095cc51d486a78ddb710033ad6e16
George Bien Almenanza <georgebien.almenanza@ph.fujitsu.com>
2021-02-03 14:15:27 +0800

    [NAVBAR] Edited: Changed the condition to what position will a navigation will appear

--- public/scripts/Login/Login.js
+++ public/scripts/Login/Login.js
@@ -33,27 +33,43 @@ const LOGIN = (() => {
             },
             success: result => {
 
-                if (parseInt(result.value.access_level) === 2) {
-                    if (result.response === 'success') {
+                if (parseInt(result.value.access_level) === 2) 
+                {
+                    if (result.response === 'success') 
+                    {
                         $('#span_error').remove();
-                        if (result.value.position === 'SECTION LEADER' || result.value.position === 'LEADER') {
+                        if (result.value.position === 'SECTION LEADER' || result.value.position === 'LEADER' || result.value.position === 'QC ASSISTANT') 
+                        {
                             location.href = 'http://localhost/tis/public/evaluation';
-                        } else if (result.value.position === 'MANAGER') {
+                        } 
+                        else if (result.value.position === 'MANAGER' || result.value.position === 'DIRECTOR') 
+                        {
                             location.href = 'http://localhost/tis/public/approval';
-                        } else {
+                        } 
+                        else 
+                        {
                             location.href = 'http://localhost/tis/public/trial-checksheet';
                         }
-                    } else if (result.response === 'fail') {
+                    } 
+                    else if (result.response === 'fail') 
+                    {
                         $('#span_error').remove();
                         $('#div_password').after(`<span id="span_error" class="text-danger">${result.value}</span>`)
-                    } else {
+                    } 
+                    else 
+                    {
                         $('#span_error').remove();
                         $('#div_password').after(`<span id="span_error" class="text-danger">An error occured. Please contact the administrator</span>`);
                     }
-                } else {
-                    if(result.response === 'success'){
+                } 
+                else 
+                {
+                    if(result.response === 'success')
+                    {
                         location.href = 'http://localhost/tis/public/trial-checksheet';
-                    } else {
+                    } 
+                    else 
+                    {
                         $('#span_error').remove();
                         $('#div_password').after(`<span id="span_error" class="text-danger">${result.value}</span>`)
                     }
